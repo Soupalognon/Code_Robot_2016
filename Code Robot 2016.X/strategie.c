c942cd961f0374c52ca86d165e7842ffdfac5504
Soupalognon <gabriel.durand@hotmail.fr>
2016-01-22 12:47:09 +0100

    Ajout code robot 2015 + Démarrage code Robot 2016

---
+++ Code Robot 2016.X/strategie.c
@@ -0,0 +1,215 @@
+/******************************************************************************/
+/************** Carte principale Robot 1 : DSPIC33FJ128MC804*******************/
+/******************************************************************************/
+/* Fichier 	: srategie.c
+ * Auteur  	: Quentin
+ * Revision	: 1.0
+ * Date		: 07 février 2015, 22:47
+ *******************************************************************************
+ *
+ *
+ ******************************************************************************/
+
+/******************************************************************************/
+/******************************** INCLUDES ************************************/
+/******************************************************************************/
+
+#include "system.h"
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+
+
+void strategie()
+{
+    COULEUR = couleur_depart();
+    
+    #ifdef GROS_ROBOT
+        // Inits avant démarage du robot :
+        init_jack();
+
+        if (COULEUR == JAUNE)
+            synchro_AX12(AX_US, 45, 1023, SANS_ATTENTE);
+        else
+            synchro_AX12(AX_US, -45, 1023, SANS_ATTENTE);
+
+        while(!SYS_JACK);
+
+        // Démarage du match
+        TIMER_90s = ACTIVE;
+        EVITEMENT_ADV_AVANT = ON;
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+
+        init_position_robot(180., 988., 0.);
+        FLAG_ACTION = INIT_ASCENSEUR;
+
+        delay_ms(1000);
+        
+    #endif
+
+        
+    #ifdef PETIT_ROBOT
+        init_position_robot (153, 1030, 0);
+        EVITEMENT_ADV_AVANT = OFF;
+
+        //Init départ
+        init_pinces_jack();
+
+        while(!SYS_JACK);
+        TIMER_90s = ACTIVE;
+
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+        FLAG_ACTION = INIT_PINCES_DEMARRAGE;       
+        
+    #endif
+}
+
+
+void homologation()
+{
+    COULEUR = couleur_depart();
+
+    #ifdef GROS_ROBOT
+
+      // Inits avant démarage du robot :
+        init_jack();
+
+        if (COULEUR == JAUNE)
+            synchro_AX12(AX_US, 45, 1023, SANS_ATTENTE);
+        else
+            synchro_AX12(AX_US, -45, 1023, SANS_ATTENTE);
+
+        while(!SYS_JACK);
+
+        // Démarage du match
+        TIMER_90s = ACTIVE;
+        EVITEMENT_ADV_AVANT = ON;
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+
+        init_position_robot(180., 988., 0.);
+        FLAG_ACTION = INIT_ASCENSEUR;
+
+        delay_ms(1000);
+
+    #endif
+
+    #ifdef PETIT_ROBOT
+
+        init_position_robot (153, 1030, 0);
+        EVITEMENT_ADV_AVANT = OFF;
+
+        //Init départ
+        init_pinces_jack();
+
+        while(!SYS_JACK);
+        TIMER_90s = ACTIVE;
+
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+        FLAG_ACTION = INIT_PINCES_DEMARRAGE;
+
+    #endif
+
+}
+
+void reglage_odometrie()
+{
+    delay_ms(2000);
+    //while(!SYS_JACK);
+    COULEUR = couleur_depart();
+    
+    #ifdef GROS_ROBOT
+        init_jack();
+    #endif
+    
+    EVITEMENT_ADV_ARRIERE = OFF;
+    EVITEMENT_ADV_AVANT = OFF;
+
+    init_position_robot (0, 0, 0);
+    init_position_robot (-100, 0, 0);
+    rejoindre(0, 0, MARCHE_AVANT, 100);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+
+//           faire_des_tours(-32);
+            while(1);
+////
+////            TIMER_DEBUG = ACTIVE;
+//            init_position_robot(0, 0, 0);
+            ////Horraire
+//       rejoindre(2000, 0, MARCHE_AVANT, 50);
+//       orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter(-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter(-90, 50);
+
+
+            //// Anti horaire
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter(90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter(90, 50);
+        rejoindre(500, 0, MARCHE_AVANT, 100);
+
+
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(500, 0, MARCHE_AVANT, 100);
+//        TIMER_DEBUG = DESACTIVE;
+
+        delay_ms(10000);
+}
