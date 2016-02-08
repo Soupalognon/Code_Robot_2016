f520bb1afd0fc4f7dff02ba1e972a93fead951d3
Soupalognon <gabriel.durand@hotmail.fr>
2016-02-08 17:41:07 +0100

    Tri - Séparations R1 et R2

---
+++ R2/Code Robot 2016.X/main.c
@@ -0,0 +1,132 @@
+/******************************************************************************/
+/************** Carte principale Robot 1 : DSPIC33FJ128MC804*******************/
+/******************************************************************************/
+/* Fichier 	: main.c
+ * Auteur  	: Quentin
+ * Revision	: 1.0
+ * Date		: 22 octobre 2015, 23:55
+ *******************************************************************************
+ *
+ *
+ ******************************************************************************/
+
+#include "system.h"
+#include <xc.h>
+
+/******************************************************************************/
+/************************ Configurations Bits *********************************/
+/******************************************************************************/
+
+// DSPIC33FJ128MC804 Configuration Bit Settings
+
+// FBS
+#pragma config BWRP = WRPROTECT_OFF     // Boot Segment Write Protect (Boot Segment may be written)
+#pragma config BSS = NO_FLASH           // Boot Segment Program Flash Code Protection (No Boot program Flash segment)
+#pragma config RBS = NO_RAM             // Boot Segment RAM Protection (No Boot RAM)
+
+// FSS
+#pragma config SWRP = WRPROTECT_OFF     // Secure Segment Program Write Protect (Secure segment may be written)
+#pragma config SSS = NO_FLASH           // Secure Segment Program Flash Code Protection (No Secure Segment)
+#pragma config RSS = NO_RAM             // Secure Segment Data RAM Protection (No Secure RAM)
+
+// FGS
+#pragma config GWRP = OFF               // General Code Segment Write Protect (User program memory is not write-protected)
+#pragma config GSS = OFF                // General Segment Code Protection (User program memory is not code-protected)
+
+// FOSCSEL
+#pragma config FNOSC = FRCPLL           // Oscillator Mode (Internal Fast RC (FRC) w/ PLL)
+#pragma config IESO = OFF //ON                // Internal External Switch Over Mode (Start-up device with FRC, then automatically switch to user-selected oscillator source when ready)
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Source (Primary Oscillator Disabled)
+#pragma config OSCIOFNC = ON            // OSC2 Pin Function (OSC2 pin has digital I/O function)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow Multiple Re-configurations)
+#pragma config FCKSM = CSECMD  //CSDCMD           // Clock Switching and Monitor (Both Clock Switching and Fail-Safe Clock Monitor are disabled)
+
+// FWDT
+#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler (1:32,768)
+#pragma config WDTPRE = PR128           // WDT Prescaler (1:128)
+#pragma config WINDIS = OFF             // Watchdog Timer Window (Watchdog Timer in Non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog timer enabled/disabled by user software)
+
+// FPOR
+#pragma config FPWRT = PWR128           // POR Timer Value (128ms)
+#pragma config ALTI2C = OFF             // Alternate I2C  pins (I2C mapped to SDA1/SCL1 pins)
+#pragma config LPOL = ON                // Motor Control PWM Low Side Polarity bit (PWM module low side output pins have active-high output polarity)
+#pragma config HPOL = ON                // Motor Control PWM High Side Polarity bit (PWM module high side output pins have active-high output polarity)
+#pragma config PWMPIN = ON              // Motor Control PWM Module Pin Mode bit (PWM module pins controlled by PORT register at device Reset)
+
+// FICD
+#pragma config ICS = PGD1               // Comm Channel Select (Communicate on PGC1/EMUC1 and PGD1/EMUD1)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG is Disabled)
+
+
+
+/******************************************************************************/
+/********************* DECLARATION DES VARIABLES GLOBALES *********************/
+/******************************************************************************/
+
+    uint8_t COULEUR;
+
+    uint8_t DETECTION;
+    uint8_t EVITEMENT_ADV_AVANT;
+    uint8_t EVITEMENT_ADV_ARRIERE;
+    uint8_t STRATEGIE_EVITEMENT;
+    uint16_t FLAG_ACTION;
+    uint8_t FLAG_EVITEMENT_STRATEGIQUE;
+    uint8_t ETAT_AUTOM;
+    uint16_t COMPTEUR_MARCHE;
+    uint8_t COMPTEUR_TEMPS_MATCH;
+    uint8_t CONFIG_COQUILLAGE;
+
+
+    int obstacle[30][20];
+	int calcul_en_cours=OFF;
+    int curseur;
+    int curseur_obstacle;
+    int itineraire[nb_point_max][3];
+    int itineraire_basique[nb_point_max][3];
+    int itineraire_court[nb_point_max][3];
+    int point_obstacle[nb_point_max][3];
+    int x_actuel;
+    int y_actuel;
+    int x_obj;
+    int y_obj;
+    int id_evitement = 0;
+    int id_evitement_initial = 0;
+    int chemin_court=0;
+    uint16_t compteur_evitement=0;
+    int precedent_obstacle[7];
+
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+
+    
+int main(int argc, char** argv)
+{
+    init_system();
+    init_evitement();
+    //TIMER_DEBUG = ACTIVE;
+    delay_ms(500);
+
+    
+    init_decalage_AX12();
+    init_position_AX12();
+
+    
+    //Init Supplémentaire
+#ifdef GROS_ROBOT
+    synchro_AX12(AX_US, 0, 1023, SANS_ATTENTE);
+#endif
+    
+
+    strategie();
+
+    //reglage_odometrie();
+    
+    while(1);
+    return (EXIT_SUCCESS);
+}
+
---
+++ R2/Code Robot 2016.X/strategie.c
@@ -0,0 +1,267 @@
+/******************************************************************************/
+/************** Carte principale Robot 1 : DSPIC33FJ128MC804*******************/
+/******************************************************************************/
+/* Fichier 	: srategie.c
+ * Auteur  	: Quentin
+ * Revision	: 1.0
+ * Date		: 07 février 2015, 22:47
+ *******************************************************************************
+ *
+ *
+ ******************************************************************************/
+
+/******************************************************************************/
+/******************************** INCLUDES ************************************/
+/******************************************************************************/
+
+#include "system.h"
+
+/******************************************************************************/
+/******************************************************************************/
+/******************************************************************************/
+
+
+void strategie()
+{
+    COULEUR = couleur_depart();
+    /*
+     * Definit la configuration des coquillages CONFIG_1 - CONFIG_5
+     */
+    CONFIG_COQUILLAGE = CONFIG_4;
+    
+    #ifdef GROS_ROBOT
+        // Inits avant démarage du robot :
+        init_jack();
+
+        if (COULEUR == JAUNE)
+            synchro_AX12(AX_US, 45, 1023, SANS_ATTENTE);
+        else
+            synchro_AX12(AX_US, -45, 1023, SANS_ATTENTE);
+
+        while(!SYS_JACK);
+
+        // Démarage du match
+        TIMER_90s = ACTIVE;
+        EVITEMENT_ADV_AVANT = ON;
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+
+        init_position_robot(180., 988., 0.);
+        FLAG_ACTION = INIT_ASCENSEUR;
+
+        delay_ms(1000);
+        
+    #endif
+
+        
+    #ifdef PETIT_ROBOT
+        init_position_robot (65, 1100, 0);
+        EVITEMENT_ADV_AVANT = OFF;
+        
+
+        //Init départ
+        //init_pinces_jack();
+
+        
+        while(SYS_JACK);
+        delay_ms(2000); 
+        while(!SYS_JACK);
+        TIMER_90s = ACTIVE;
+
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+        
+            synchro_AX12(PORTE_D, 90, 1023, SANS_ATTENTE);
+            delay_ms(1000);
+            synchro_AX12(PORTE_G, -90, 1023, SANS_ATTENTE);
+            delay_ms(2000);
+        
+        /*
+         * Pousser blocs du milieu jusqu'a la zone
+         */
+        
+            passe_part(550, 1100, MARCHE_AVANT, 100, DEBUT_TRAJECTOIRE);
+            passe_part(1200, 1100, MARCHE_AVANT, 70, FIN_TRAJECTOIRE);
+            rejoindre(1050, 1100, MARCHE_ARRIERE, 100);
+            FLAG_ACTION=OUVRIR_PORTES;
+            
+        /*
+         * Ecrire deplacement pour chacune des configurations possibles
+         * utiliser : FLAG_ACTION = FERMER_PORTES pour fermer les portes
+         */
+        
+        switch(CONFIG_COQUILLAGE)
+        {
+            case CONFIG_1:
+
+                break;
+            case CONFIG_2:
+                break;
+            case CONFIG_3:
+                break;
+            case CONFIG_4:
+                passe_part(1200, 350, MARCHE_AVANT, 100, DEBUT_TRAJECTOIRE);
+                passe_part(700, 450, MARCHE_AVANT, 100, MILIEU_TRAJECTOIRE);
+                passe_part(250, 450, MARCHE_AVANT, 100, MILIEU_TRAJECTOIRE);
+                passe_part(250, 750, MARCHE_AVANT, 100, MILIEU_TRAJECTOIRE);
+                passe_part(250, 810, MARCHE_AVANT, 100, FIN_TRAJECTOIRE);
+                
+                break;
+            case CONFIG_5:
+                break;
+            default:
+                break;
+        }
+        
+        /*
+         * Strategie lever de drapeaux
+         */
+        
+    #endif
+}
+
+
+void homologation()
+{
+    COULEUR = couleur_depart();
+
+    #ifdef GROS_ROBOT
+
+      // Inits avant démarage du robot :
+        init_jack();
+
+        if (COULEUR == JAUNE)
+            synchro_AX12(AX_US, 45, 1023, SANS_ATTENTE);
+        else
+            synchro_AX12(AX_US, -45, 1023, SANS_ATTENTE);
+
+        while(!SYS_JACK);
+
+        // Démarage du match
+        TIMER_90s = ACTIVE;
+        EVITEMENT_ADV_AVANT = ON;
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+
+        init_position_robot(180., 988., 0.);
+        FLAG_ACTION = INIT_ASCENSEUR;
+
+        delay_ms(1000);
+
+    #endif
+
+    #ifdef PETIT_ROBOT
+
+        init_position_robot (153, 1030, 0);
+        EVITEMENT_ADV_AVANT = OFF;
+
+        //Init départ
+        init_pinces_jack();
+
+        while(!SYS_JACK);
+        TIMER_90s = ACTIVE;
+
+        STRATEGIE_EVITEMENT = EVITEMENT_NORMAL;
+
+    #endif
+
+}
+
+void reglage_odometrie()
+{
+    delay_ms(2000);
+    //while(!SYS_JACK);
+    COULEUR = couleur_depart();
+    
+    #ifdef GROS_ROBOT
+        init_jack();
+    #endif
+    
+    EVITEMENT_ADV_ARRIERE = OFF;
+    EVITEMENT_ADV_AVANT = OFF;
+
+    init_position_robot (0, 0, 0);
+    init_position_robot (-100, 0, 0);
+    rejoindre(0, 0, MARCHE_AVANT, 100);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+    trapeze(MARCHE_AVANT);
+
+//           faire_des_tours(-32);
+            while(1);
+////
+////            TIMER_DEBUG = ACTIVE;
+//            init_position_robot(0, 0, 0);
+            ////Horraire
+//       rejoindre(2000, 0, MARCHE_AVANT, 50);
+//       orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter(-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter (-90, 50);
+//        rejoindre(2000, 0, MARCHE_AVANT, 50);
+//        orienter(90, 50);
+//        rejoindre(300, 0, MARCHE_AVANT, 50);
+//        orienter(-90, 50);
+
+
+            //// Anti horaire
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter(90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter (90, 50);
+        rejoindre(2000, 0, MARCHE_AVANT, 50);
+        orienter(-90, 50);
+        rejoindre(300, 0, MARCHE_AVANT, 50);
+        orienter(90, 50);
+        rejoindre(500, 0, MARCHE_AVANT, 100);
+
+
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(2000, 0, MARCHE_AVANT, 100);
+//        rejoindre(300, 0, MARCHE_AVANT, 100);
+//        rejoindre(500, 0, MARCHE_AVANT, 100);
+//        TIMER_DEBUG = DESACTIVE;
+
+        delay_ms(10000);
+}
